408a0d320fdd2eded1259592668d8c22faab9011
Jay McCarthy <jay.mccarthy@gmail.com>
2019-06-25 16:09:48 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# gamejam-2019 --- Support code for RacketCon 2019 Game Jam tutorial
---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "gamejam-2019")
+(define deps '("base"))
+(define build-deps '())
+(define version "0.0")
+(define pkg-authors '(jay))
